0478d8474135d9d2d5ac43a3167abe31adde3494
Álvaro Carvalho <alvaro.carvalhohdr@gmail.com>
2024-09-25 22:50:35 -0300

    Primeiro commit

---
+++ website/index.php
@@ -0,0 +1,9 @@
+<?php
+$nomePrimarioTitulo = 'Página';
+$pagina = 'inicial';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/website/includes/header.php';
+?>
+
+<?php
+    require_once $_SERVER["DOCUMENT_ROOT"] . '/website/includes/footer.php';
+?>
