ad17a0796dc2c336d9aba932a9e31ac398a86bac
Álvaro Carvalho <alvaro.carvalhohdr@gmail.com>
2024-10-03 01:38:12 -0300

    ofertas-home-navbar-colapso

--- website/js/paginas/index.js
+++ website/js/paginas/index.js
@@ -1,3 +1,33 @@
 $(document).ready(function () {
-    // alert('Olá mundo index');
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: ["<img src='/website/img/index/arrow-left.svg'>", "<img src='/website/img/index/arrow-right.svg'>"],
+        dots: false,
+        dotsData: true,
+        dotsContainer: '.owl-dots',
+        startPosition: 1,
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+    $(".banners-home > .aligner > .owl-dots > button.owl-dot").click(function () {
+        let itens = $('.banners-home > .aligner > .owl-dots > button.owl-dot');
+        for(let i = 0; i < itens.length; i++) {
+            $(itens[i]).removeClass('active');
+        }
+        $(this).addClass('active');
+    });
 });
--- website/js/scripts.js
+++ website/js/scripts.js
@@ -4,4 +4,21 @@ $(document).ready(function () {
             $.getScript("/website/js/paginas/index.js");
             break;
     }
+
+    $('#navbar').on('shown.bs.collapse', function () {
+        $('body').css('overflow', 'hidden');
+    });
+
+    $('#navbar').on('hidden.bs.collapse', function () {
+        $('body').css('overflow', 'auto');
+    });
+});
+
+//efeito navbar superior
+$(window).on('scroll', function () {
+    if ($(window).scrollTop() === 0) {
+        $('#navbar').css('padding', '15px 0');
+    } else {
+        $('#navbar').css('padding', '5px 0');
+    }
 });
