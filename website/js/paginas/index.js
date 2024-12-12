27a3313e76d2037683ff99295cf546cf7a376e64
Álvaro Carvalho <alvaro.amaral_dev@soollar.com.br>
2024-12-11 21:16:13 -0300

    ajuste observer index

--- website/js/paginas/index.js
+++ website/js/paginas/index.js
@@ -67,38 +67,36 @@ $(document).ready(function () {
     });
 });
 
-window.addEventListener("load", function() {
-    const observador = (function () {
-        const init = function () {
-            createObserver();
+const observador = (function () {
+    const init = function () {
+        createObserver();
+    };
+    const createObserver = function () {
+        let options = {
+            root: null,
+            rootMargin: "250px",
+            threshold: 0.5
         };
-        const createObserver = function () {
-            let options = {
-                root: null,
-                rootMargin: "250px",
-                threshold: 0.5
-            };
-            let observer = new IntersectionObserver(
-                function (entries, observer) {
-                    handleIntersect(entries, observer);
-                },
-                options);
-            let targetElements = document.querySelectorAll("[data-animation='true']");
-            targetElements.forEach((targetElement) => {
-                observer.observe(targetElement);
-            });
-        };
-        const handleIntersect = function (entries, observer) {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add("animacao");
-                    observer.unobserve(entry.target);
-                }
-            });
-        };
-        return {
-            init: init
-        };
-    })();
-    observador.init();
-});
+        let observer = new IntersectionObserver(
+            function (entries, observer) {
+                handleIntersect(entries, observer);
+            },
+            options);
+        let targetElements = document.querySelectorAll("[data-animation='true']");
+        targetElements.forEach((targetElement) => {
+            observer.observe(targetElement);
+        });
+    };
+    const handleIntersect = function (entries, observer) {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("animacao");
+                observer.unobserve(entry.target);
+            }
+        });
+    };
+    return {
+        init: init
+    };
+})();
+observador.init();
